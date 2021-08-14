5fccafbe7b8e5d5e6c441d17aad1ee36ca1dfb0a
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-08-14 20:26:37 +0300

    deneme push

--- SehirRehberiAPI/DataAccess/DataContext.cs
+++ SehirRehberiAPI/DataAccess/DataContext.cs
@@ -17,6 +17,6 @@ namespace SehirRehberiAPI.DataAccess
         public DbSet<City> Cities { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<User> Users { get; set; }
-
+         
     }
 }
